d67b27ae72a3c4ce6496cf6fba0763539cdeebb6
Gonzalo Peci <weedv2@outlook.com>
2015-08-28 16:09:18 +1200

    20_Set inital config is already chowning the folder

--- init/10_new_user.sh
+++ init/10_new_user.sh
@@ -4,7 +4,7 @@
 
 if [ ! "$(id -u abc)" -eq "$PUID" ]; then usermod -o -u "$PUID" abc ; fi
 if [ ! "$(id -g abc)" -eq "$PGID" ]; then groupmod -o -g "$PGID" abc ; fi
-chown abc:abc -R /config
+
 echo "
 -----------------------------------
 GID/UID
